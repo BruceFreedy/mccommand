78a5c6cb70a7dffecedecf1802f4e66605dd26ef
Bruce0203 <BruceFreedy@gmail.com>
2022-04-16 10:15:24 +0900

    add spigot-api dependency

--- mccommand-core/build.gradle.kts
+++ mccommand-core/build.gradle.kts
@@ -1,1 +1,16 @@
-dependencies { api(project(":mccommand-api")) }
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.dmulloy2.net/repository/public/") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/public/") }
+    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://libraries.minecraft.net/") }
+
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    api(project(":mccommand-api"))
+}
+
